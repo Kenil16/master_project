f3af80fa389a0329bb9b541f463d4f619ecb7b28
kenil16 <kenil16@student.sdu.dk>
2020-09-24 18:16:34 +0200

    Init script with stable PX4 version 1.11.0 added

--- software/ros_workspace/init_setup.bash
+++ software/ros_workspace/init_setup.bash
@@ -7,12 +7,19 @@ ros_workspace="$PWD"
 cd ~
 mkdir PX4_SITL
 cd PX4_SITL
-git clone https://github.com/PX4/Firmware.git
+git clone https://github.com/PX4/Firmware.git --branch v1.11.0
 cd Firmware
 git submodule update --init --recursive
 
-#Place 3d models in the PX4 firmware (-R recursively and -T not source directory)
+#Place 2d models in the PX4 firmware (-R recursively and -T not source directory)
 cd $ros_workspace
-cp -RT 3D-models/models/ /home/$USER/PX4_SITL/Firmware/Tools/sitl_gazebo/models
-cp -RT 3D-models/worlds/ /home/$USER/PX4_SITL/Firmware/Tools/sitl_gazebo/worlds
+cp -RT PX4-software/models/ /home/$USER/PX4_SITL/Firmware/Tools/sitl_gazebo/models
+cp -RT PX4-software/worlds/ /home/$USER/PX4_SITL/Firmware/Tools/sitl_gazebo/worlds
+cp -RT PX4-software/init.d-posix /home/$USER/PX4_SITL/Firmware/ROMFS/px4fmu_common/init.d-posix/
+cp -RT PX4-software/mixers /home/$USER/PX4_SITL/Firmware/ROMFS/px4fmu_common/mixers
 
+#Build the PX4 SITL firmware
+cd /home/$USER/PX4_SITL/Firmware
+DONT_RUN=1 make px4_sitl_default gazebo
+
+cd $ros_workspace
