fd2a0a160140e2ebe2cd52ca407ff826e6a9d013
kenil16 <kenil16@student.sdu.dk>
2020-10-02 17:20:03 +0200

    Transformation matrix problems fixed and Kalman filter initialized on ArUco marker pose

--- software/ros_workspace/src/marker_detection/marker_detection.py
+++ software/ros_workspace/src/marker_detection/marker_detection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import cv2
+import matplotlib as plt
 import numpy as np
 import rospy
 from os import system
@@ -13,15 +14,37 @@ import tf2_geometry_msgs
 from geometry_msgs.msg import PoseStamped, Quaternion
 from std_msgs.msg import Float64, Bool
 
+from tracking import*
+
 class marker_detection:
 
     def __init__(self):
 
+        #Init ROS node
         rospy.init_node('marker_detection')
+
+        #Init Kalman filters
+        self.kf_pos = kalman_filter()
+        self.kf_ori = kalman_filter()
+
+        #Init data test plotting
+        self.plot_aruco_pos_x = []
+        self.plot_aruco_pos_y = []
+        self.plot_aruco_pos_z = []
+
+        self.plot_aruco_pos_kf_x = []
+        self.plot_aruco_pos_kf_y = []
+        self.plot_aruco_pos_kf_z = []
+
+        self.cycle_time = (1./50.)
+        self.plot_timer = int(((20)/self.cycle_time)) #Set to run 10 seconds before plot
+
+        self.plot_time = []
         
         #Local drone pose
         self.local_position = PoseStamped()
         self.aruco_pose = PoseStamped()
+        self.aruco_pose_without_kf = PoseStamped()
         
         #Subscribers
         self.image_sub = rospy.Subscriber("/mono_cam/image_raw", Image, self.find_aruco_markers)
@@ -29,8 +52,8 @@ class marker_detection:
 
         #Publishers
         self.aruco_marker_image_pub = rospy.Publisher("aruco_marker_image", Image, queue_size=1)
-        self.aruco_marker_pose_pub = rospy.Publisher("marker_pose", PoseStamped, queue_size=1)
-        
+        self.aruco_marker_pose_pub = rospy.Publisher("/mavros/vision_pose/pose", PoseStamped, queue_size=1)
+
         #self.tf_buffer = tf2_ros.Buffer(rospy.Duration(1200.0)) #tf buffer length
         #self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
@@ -43,7 +66,7 @@ class marker_detection:
         self.marker_pose = []
         self.bridge = CvBridge()
         
-        rospy.Timer(rospy.Duration(2), self.timer_callback)
+        rospy.Timer(rospy.Duration(self.cycle_time), self.timer_callback)
         rospy.spin()
 
     def local_position_callback(self, data):
@@ -99,59 +122,75 @@ class marker_detection:
             
             if pose[0] == marker_id:
                 
+                """
                 #Transformation matrix from world to drone
                 x = self.local_position.pose.orientation.x
                 y = self.local_position.pose.orientation.y
                 z = self.local_position.pose.orientation.z
                 w = self.local_position.pose.orientation.w
-
+                
                 T_world_drone = quaternion_matrix(np.array([x,y,z,w]))
+                
+                T_world_drone = euler_matrix(0,0,np.pi,'rxyz')
+                
                 T_world_drone[0][3] = self.local_position.pose.position.x
                 T_world_drone[1][3] = self.local_position.pose.position.y
                 T_world_drone[2][3] = self.local_position.pose.position.z
+                """
 
                 #Transformation matrix from drone to camera
-                T_drone_camera = euler_matrix(-1.5707,1.5707,0)
-                
+                T_drone_camera = euler_matrix(-np.pi/2, np.pi/2,0,'rxyz')
+
                 #Transformation matrix from camera to ArUco marker
                 r = quaternion_matrix(pose[1].pose.orientation)
                 T_camera_marker = np.linalg.inv(r)
                 t = np.array([pose[1].pose.position.x, pose[1].pose.position.y, pose[1].pose.position.z, 1])
                 t = -np.dot(t,T_camera_marker)
 
+                T_camera_marker = euler_matrix(0,0,0,'rxyz')
                 T_camera_marker[0][3] = t[0]
                 T_camera_marker[1][3] = t[1]
                 T_camera_marker[2][3] = t[2]
 
-                """
-                T_camera_marker[0][3] = pose[1].pose.position.x
-                T_camera_marker[1][3] = pose[1].pose.position.y
-                T_camera_marker[2][3] = pose[1].pose.position.z
+                #Transformation matrix from drone to camera
+                T_drone_marker = np.dot(T_drone_camera, T_camera_marker)
                 
-                #Now find translation of ArUco marker with respect to the camera
-                r = quaternion_matrix(pose[1].pose.orientation)
-                r_inverse = np.linalg.inv(r)
-                t = np.array([pose[1].pose.position.x, pose[1].pose.position.y, pose[1].pose.position.z, 1])
-                t = -np.dot(t,r_inverse)
-                #print(t)
+                #Get euler for and update Kalman filter
+                euler = euler_from_matrix(T_drone_marker,'rxyz')
 
-                T_drone_camera[0][3] = t[0]
-                T_drone_camera[1][3] = t[1]
-                T_drone_camera[2][3] = t[2]
-                """
+                self.kf_pos.get_measurement([T_drone_marker[0][3],T_drone_marker[1][3],T_drone_marker[2][3]])
+                self.kf_ori.get_measurement([euler[0],euler[1],euler[2]])
+                 
+                self.aruco_pose.pose.position.x = self.kf_pos.tracker.x[0]
+                self.aruco_pose.pose.position.y = self.kf_pos.tracker.x[1]
+                self.aruco_pose.pose.position.z = self.kf_pos.tracker.x[2]
 
-                #Transformation matrix from world to camera
-                T_world_marker = np.matmul(np.matmul(T_world_drone,T_drone_camera),T_camera_marker)
-                print "Marker id: {} Orientation: {} x: {} y: {} z: {} \n".format(pose[0],euler_from_matrix(T_world_marker),T_world_marker[0][3],T_world_marker[1][3],T_world_marker[2][3])
-                
-                #self.aruco_marker_pose_pub.publish(self.aruco_pose)
-                
-                #print("T_world_: " + str(T_world_camera))
-                #print(T)
-                
-                #print "Marker id: {} Position: {} Orientation: {} \n".format(pose[0],pose[1].pose.position,eular_angles)
+                self.aruco_pose.pose.orientation = Quaternion(*quaternion_from_euler(self.kf_ori.tracker.x[0],self.kf_ori.tracker.x[1],self.kf_ori.tracker.x[2],'rxyz'))
+                self.aruco_marker_pose_pub.publish(self.aruco_pose)
 
+                #Plot position data (Just for testing)
+                if self.plot_timer > 0:
+                    self.plot_timer -= 1
 
+                    self.plot_aruco_pos_x.append(T_drone_marker[0][3])
+                    self.plot_aruco_pos_y.append(T_drone_marker[1][3])
+                    self.plot_aruco_pos_z.append(T_drone_marker[2][3])
+
+                    self.plot_aruco_pos_kf_x.append(self.kf_pos.tracker.x[0])
+                    self.plot_aruco_pos_kf_y.append(self.kf_pos.tracker.x[1])
+                    self.plot_aruco_pos_kf_z.append(self.kf_pos.tracker.x[2])
+                    
+                    if not self.plot_time:
+                        self.plot_time.append(self.cycle_time)
+                    else:
+                        self.plot_time.append(self.cycle_time + self.plot_time[-1])
+
+                    if not self.plot_timer:
+                        self.plot_data(self.plot_aruco_pos_x,self.plot_aruco_pos_kf_x,self.plot_time,'Estimated ArUco marker position','Time [s]','x [m]','../../../../data/plots/kf_pos_x.png')
+                        self.plot_data(self.plot_aruco_pos_y,self.plot_aruco_pos_kf_y,self.plot_time,'Estimated ArUco marker position','Time [s]','y [m]','../../../../data/plots/kf_pos_y.png')
+                        self.plot_data(self.plot_aruco_pos_z,self.plot_aruco_pos_kf_z,self.plot_time,'Estimated ArUco marker position','Time [s]','z [m]','../../../../data/plots/kf_pos_z.png')
+
+                print "Marker id: {} Ori: {} x: {} y: {} z: {} \n".format(pose[0],euler,self.aruco_pose.pose.position.x,self.aruco_pose.pose.position.y,self.aruco_pose.pose.position.z)
             
 
     def rodrigues_to_euler_angles(self, rvec):
@@ -189,6 +228,33 @@ class marker_detection:
         R = np.dot(R_z, np.dot( R_y, R_x ))
         
         return R
+
+    def plot_data(self,without_kf,with_kf,time,title,xlabel,ylabel,fig_path_name):
+        
+        fig, ax = plt.subplots()
+        ax.set_axisbelow(True)
+        ax.set_facecolor('#E6E6E6')
+        plt.grid(color='w', linestyle='solid')
+
+        for spine in ax.spines.values():
+            spine.set_visible(False)
+
+        ax.xaxis.tick_bottom()
+        ax.yaxis.tick_left()
+        ax.tick_params(colors='gray', direction='out')
+        
+        for tick in ax.get_xticklabels():
+            tick.set_color('gray')
+        for tick in ax.get_yticklabels():
+            tick.set_color('gray')
+
+        ax.scatter(time,without_kf,label='Data points')
+        ax.plot(time,with_kf,label='Kalman filter',color='red',markersize=2)
+        ax.set_title(title)
+        ax.legend()
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.savefig(fig_path_name)
     
     def timer_callback(self,event):
         self.estimate_marker_pose(100)
---
+++ software/ros_workspace/src/marker_detection/tracking.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python2
+
+#from __future__ import print_function
+import numpy as np
+#import cv2
+import matplotlib.pyplot as plt
+from filterpy.kalman import KalmanFilter 
+from filterpy.common import Q_discrete_white_noise
+from scipy.linalg import block_diag
+#from numpy.random import randn
+from filterpy import*
+
+class kalman_filter:
+
+    def __init__(self):
+        
+        #Kalman filter 
+        self.is_state_initialized = False
+
+        self.R_std = 0.34#30 #0.35
+        self.Q_std = 0.04 #0.04
+
+        self.tracker = KalmanFilter(dim_x=6,dim_z=3)
+        self.dt = 0.2#Five frames each second
+        self.tracker.F = np.array([[1,0,0,self.dt,0,0],[0,1,0,0,self.dt,0],[0,0,1,0,0,self.dt],[0,0,0,1,0,0],[0,0,0,0,1,0],[0,0,0,0,0,1]])
+        
+        self.tracker.R = np.eye(3) * self.R_std**2
+        self.q = Q_discrete_white_noise(dim=3, dt=self.dt, var=self.Q_std**2)
+        self.tracker.Q = block_diag(self.q, self.q)
+        
+        """
+        self.tracker.Q[0,0] = q[0,0]
+        self.tracker.Q[1,1] = q[0,0]
+        self.tracker.Q[2,2] = q[1,1]
+        self.tracker.Q[3,3] = q[1,1]
+        self.tracker.Q[0,2] = q[0,1]
+        self.tracker.Q[2,0] = q[0,1]
+        self.tracker.Q[1,3] = q[0,1]
+        self.tracker.Q[3,1] = q[0,1]
+        """
+
+        #Was set to np.array([[1, 0, 0, 0, 0, 0],[0, 1, 0, 0, 0, 0],[0, 0, 1, 0, 0, 0])
+        self.tracker.H = np.array([[1, 0, 0, 0, 0, 0],[0, 1, 0, 0, 0, 0],[0, 0, 1, 0, 0, 0]])
+        self.tracker.x = np.array([[0, 0, 0, 0, 0, 0]]).T
+        self.tracker.P = np.eye(6) * pow(500,2)
+        
+    def first_update(self,data):
+    
+        #Init values for Kalman filtering
+        self.is_state_initialized = True
+
+        self.tracker.R = np.eye(3) * self.R_std**2
+        self.tracker.P = np.eye(6) * 500.
+        self.tracker.x = np.array([data[0], data[1], data[2], 0, 0, 0]).T
+        
+        self.tracker.predict()
+        self.tracker.update(data)
+    
+    def get_measurement(self, data):
+
+        #Initialize kalman filter
+        if not self.is_state_initialized:
+            self.first_update(data)
+        else:
+            self.tracker.predict()
+            self.tracker.update(data)
+
+if __name__ == "__main__":
+
+        #This is only for testing of the Kalman filter
+        kf = kalman_filter()
+    
+        pos, vel = (0, 0), (2, 0.5)
+        sensor = kf.init_pos(pos, vel, noise_std=1)
+        
+        ps1 = np.array([kf.read() for _ in range(30)])
+        pos, vel = (0, 0), (2, 0.5)
+        sensor = kf.init_pos(pos, vel, noise_std=1)
+
+        ps2 = np.array([kf.read() for _ in range(30)])
+        
+        pt0 = []
+        pt0.append(ps1[0])
+        pt0.append(ps2[0])
+
+        kf.get_measurement(pt0)
+
+        x1, y1, kfx1, kfy1 = [], [], [], []
+        x2, y2, kfx2, kfy2 = [], [], [], []
+        
+
+        for z in range(29):
+
+                points = []
+                points.append(ps1[z+1])
+                points.append(ps2[z+1])
+            
+                kf_trace = kf.get_measurement(points)
+                
+                kfx1.append(kf.x[0][0])
+                kfy1.append(kf.x[0][1])
+                x1.append(ps1[z][0])
+                y1.append(ps1[z][1])
+
+                kfx2.append(kf.x[1][0])
+                kfy2.append(kf.x[1][1])
+                x2.append(ps2[z][0])
+                y2.append(ps2[z][1])
+
+
+        fig, ax = plt.subplots()
+        ax.scatter(x1, y1)
+        ax.scatter(x2, y2)
+        ax.plot(kfx1,kfy1)
+        ax.plot(kfx2,kfy2)
+        ax.set(xlabel='Position x [m]', ylabel='Position y [m]',title='2D Position tracking Kalman filter')
+        ax.grid()
+        fig.savefig("test.png")
+
